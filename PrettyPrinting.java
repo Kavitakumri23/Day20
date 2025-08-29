3a5cf582b11e9bac6cbc954a894706cd34a6f9ed
Kavitakumri23 <kroy22519@gmail.com>
2025-08-29 22:12:44 +0530

    String

---
+++ PrettyPrinting.java
@@ -0,0 +1,18 @@
+//Placeholder in string And operators
+import java.util.Arrays;
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+//        System.out.printf("Formatted number is %.2f", a);
+//        System.out.println(Math.PI);
+        System.out.printf("Pie: %.3f", Math.PI);
+        System.out.println();
+        System.out.printf("Hello my name is %s and I am %s", "Kavita", "Cool");
+        System.out.println();
+        System.out.println("a" + "b");
+        System.out.println('a' + 'b');
+        System.out.println('a' + 3);
+//        System.out.println("Kunal" + new ArrayList<>());
+    }
+
+}
